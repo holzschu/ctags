3f8688d1e0333d1f3a00b0093149a76cad28d27e
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2018-03-11 16:51:50 +0100

    Get frameworks / includes for compilation

---
+++ get_frameworks.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+IOS_SYSTEM_VER="1.1"
+LIBSSH2_VER="1.0"
+
+HHROOT="https://github.com/holzschu"
+
+(cd "${BASH_SOURCE%/*}/Frameworks"
+# ios_system
+echo "Downloading ios_system, openSSL and libSSH2:"
+curl -OL $HHROOT/ios_system/releases/download/v$IOS_SYSTEM_VER/smallRelease.tar.gz
+( tar -xzf smallRelease.tar.gz --strip 1 && rm smallRelease.tar.gz ) || { echo "ios_system failed to download"; exit 1; }
+)
+
+echo "Downloading header file:"
+curl -OL $HHROOT/ios_system/releases/download/v$IOS_SYSTEM_VER/ios_error.h 
+
+
+
+
