51c5631d0e424f219b58c73de7747b2e0e2ca6b6
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2018-03-19 20:17:02 +0100

    Project added to git

---
+++ ctags/ctags/ctags.h
@@ -0,0 +1,13 @@
+//
+//  ctags.h
+//  ctags
+//
+//  Created by Nicolas Holzschuch on 25/02/2018.
+//  Copyright © 2018 Nicolas Holzschuch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ctags : NSObject
+
+@end
---
+++ ctags/ctags/ctags.m
@@ -0,0 +1,13 @@
+//
+//  ctags.m
+//  ctags
+//
+//  Created by Nicolas Holzschuch on 25/02/2018.
+//  Copyright © 2018 Nicolas Holzschuch. All rights reserved.
+//
+
+#import "ctags.h"
+
+@implementation ctags
+
+@end
